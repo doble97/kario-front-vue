f63e8e2ca3331f099681b2db580895d99d723fe6
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2024-10-26 17:27:44 +0200

    Added tailwindcss and daisyui

--- src/modules/landing/pages/AboutPage.vue
+++ src/modules/landing/pages/AboutPage.vue
@@ -1,5 +1,10 @@
 <template>
   <div>About Page</div>
+  <button
+    class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+  >
+    Button
+  </button>
 </template>
 
 <script setup lang="ts"></script>
